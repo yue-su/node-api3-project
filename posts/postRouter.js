8a5fca03071ab9a9e0dc971a51014fe75fb8cc73
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-09-02 17:49:06 -0300

    added getbyID

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -16,7 +16,11 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  posts.getById(req.params.id)
+    .then(post => {
+    post ? res.status(200).json(post) : res.status(404).json({message: 'post not found'})
+    })
+  .catch(err => res.status(500).json({message: 'Error retriving the post'}))
 });
 
 router.delete('/:id', (req, res) => {
