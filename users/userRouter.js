85ff9a2fd5f0885d97a9e1ad10335bab06e61254
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-09-02 21:57:58 -0300

    added validateduserid

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,59 @@
-const express = require('express');
+const express = require("express")
 
-const router = express.Router();
+const users = require("./userDb")
+const router = express.Router()
 
-router.post('/', (req, res) => {
+router.post("/", validateUser, (req, res) => {
   // do your magic!
-});
+})
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", (req, res) => {
   // do your magic!
-});
+})
 
-router.get('/', (req, res) => {
-  // do your magic!
-});
+router.get("/", (req, res) => {
+  users
+    .get(req.query)
+    .then((users) => res.status(200).json(users))
+    .catch((err) => res.status(500).json({ message: "Error retriving users" }))
+})
 
-router.get('/:id', (req, res) => {
+router.get("/:id", validateUserId, (req, res) => {
+  res.status(200).json(req.user)
   // do your magic!
-});
+})
 
-router.get('/:id/posts', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
-});
+})
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
-});
+})
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
-});
+})
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const id = req.params.id
+  users.getById(id)
+    .then(user => {
+      if (user) {
+        req.user = user
+        next()
+      } else res.status(400).json({ message: 'invalid user id' })
+  }).catch(err => res.status(500).json({message: 'Error retriving user'}))
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  
 }
 
 function validatePost(req, res, next) {
   // do your magic!
 }
 
-module.exports = router;
+module.exports = router
